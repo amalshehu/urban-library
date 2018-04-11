2a13f92e189b35a505874e5776884240ef50cc72
amalshehu <amalshehu@gmail.com>
2018-04-11 14:10:56 +0530

    Add mongodb and mongoose

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var coolUserRouter = require('./routes/cool')
 
 var app = express()
 
+//Set up mongoose connection
+var mongoose = require('mongoose')
+var mongoDB = ''
+mongoose.connect(mongoDB)
+mongoose.Promise = global.Promise
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
